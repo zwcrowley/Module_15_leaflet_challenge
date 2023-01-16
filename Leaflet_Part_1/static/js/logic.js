96139b680f63c2fa6538d7294ba13e8715a64da2
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-15 20:51:21 -0600

    Map not working

--- Leaflet_Part_1/static/js/logic.js
+++ Leaflet_Part_1/static/js/logic.js
@@ -1,12 +1,15 @@
 // Leaflet challenge Part 1:
 // By: Zack Crowley
+// Create a map of all the earthquakes in the past week in the US:
 
+// Run an API call from: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+let usgs_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function createMap(bikeStations) {
+// createMap takes the markers array made below and creates a basemap and overlay- overlay has the markers:
+function createMap(earthQuakes) {
 
 	let USGS_USTopo = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}', {
 								maxZoom: 20, attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'});
-  
   
 	// Create a baseMaps object to hold the USGS_USTopo layer.
 	let baseMaps = {
@@ -29,40 +32,52 @@ function createMap(bikeStations) {
 	L.control.layers(baseMaps, overlayMaps, {
 	  collapsed: false
 	}).addTo(map);
-  }
   
-  function createMarkers(response) {
+}
+  
+function createMarkers(response) {
   
 	// Pull the "stations" property from response.data.
-	let eqs = response.data.stations;
+	let eqs = response.features;
+	console.log("All EQS", eqs);
   
 	// Initialize an array to hold the bike markers.
 	let eqMarkers = [];
   
-	// Loop through the stations array.
-	for (let index = 0; index < eqs.length; index++) {
+	// Loop through the ea array.                  NOT WORKING!!!!!!!!!!!!!!
+	for (var index = 0; index < eqs.length; index++) {
 		let eq = eqs[index];
-  
-	  // For each station, create a marker, and bind a popup with the station's name.
-	  let eqMarker = L.circleMarker([eq.lat, eq.lon])
-		.bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-  
-	  // Add the marker to the eqMarkers array.
-	  eqMarkers.push(eqMarker);
-	}
-  
-	// Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-	createMap(L.layerGroup(bikeMarkers));
-  }
-  
-  
-  // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(createMarkers);
-  
-// USGS topo tile
 
-let USGS_USTopo = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}', {
-	maxZoom: 20,
-	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
-});
+		console.log("eq lat", eq.geometry.coordinates[2]);  
+		console.log("eq long", eq.geometry.coordinates[1]);  
+		console.log("eq mag", eq.properties.mag);  
+		console.log("eq depth", eq.geometry.coordinates[2]);  
+		console.log("eq ids", eq.properties.ids);  
+		console.log("eq time", eq.properties.time);  
+		console.log("eq location", eq.properties.place);  
+
+		// For each earthquake, create a marker, and bind a popup with the earthquake's ids, time, and location (place):
+		L.circle([eq.geometry.coordinates[1], eq.geometry.coordinates[0]], {
+			fillOpacity:1,
+			color:eq.properties.mag,
+			fillColor:eq.properties.mag,
+			radius:eq.geometry.coordinates[2] // depth is the third coordinate in geometry
+			}).addTo(map).bindPopup(`<h1>${eq.properties.ids}</h1><hr><h3>Time: ${eq.properties.time}</h3><hr><h3>Location: ${eq.properties.place}</h3>`)
+		}
+
+
+	// Create a layer group that's made from the eqMarkers array, and pass it to the createMap function.
+	createMap(L.layerGroup(eqMarkers));
+	
+}
+  
+  
+// Perform an API call to the USGS API to get the earthquakes info for all eq in the past week, then call createMarkers when it completes.
+d3.json(usgs_url).then(createMarkers);
+
+// d3.json(usgs_url).then(function(data) { 
+// 	console.log("all data", data.features);  
+// } 
+// );
+
 
